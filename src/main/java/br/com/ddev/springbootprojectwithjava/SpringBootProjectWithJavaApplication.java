a7e4db6cc11e83578b691fc80b50fcaf3a7db544
Denilo Diniz <denilo_diniz@outlook.com>
2022-09-08 00:21:35 -0300

    Created project

---
+++ src/main/java/br/com/ddev/springbootprojectwithjava/SpringBootProjectWithJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ddev.springbootprojectwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ddev/springbootprojectwithjava/SpringBootProjectWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ddev.springbootprojectwithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
